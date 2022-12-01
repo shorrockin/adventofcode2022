a3372c7d934bd4f46d11bcd280c1f29a6725ab1e
Chris Shorrock <cs@stripe.com>
2022-12-01 06:15:51 -0800

    cleaned up input parsing

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -10,37 +10,26 @@ fn read_input_file() -> String {
     fs::read_to_string("input.txt").expect("oops - file could not be read")
 }
 
-fn parse(input: &str) -> Vec<Vec<usize>> {
-    let mut elfs: Vec<Vec<usize>> = vec![];
-    let mut current: Vec<usize> = vec![];
-    for line in input.lines() {
-        if line.trim().is_empty() {
-            elfs.push(current);
-            current = vec![];
-        } else {
-            current.push(line.trim().parse().expect(&format!("could not parse int from string: {}", line)));
-        }
-    }
-    elfs.push(current);
-    elfs
-}
-
-fn parse_calories(input: &str) -> Vec<usize> {
-    parse(input)
-        .iter()
-        .map(|elf| elf.iter().sum())
+fn parse(input: &str) -> Vec<usize> {
+    input
+        .trim() // clean up test input
+        .split("\n\n")
+        .map(|group| group 
+             .split("\n")
+             .map(|value| value.parse::<usize>().unwrap())
+             .sum())
         .collect()
 }
 
 fn part1(input: &str) -> usize {
-    *parse_calories(input)
+    *parse(input)
         .iter()
         .max()
         .expect("could not determine max value, no elements?")
 }
 
 fn part2(input: &str) -> usize {
-    let mut calories = parse_calories(input);
+    let mut calories = parse(input);
     calories.sort_by(|a, b| b.cmp(a));
 
     calories
