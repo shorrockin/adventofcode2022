b927a48501481923703839f2f8ae5e9365de484f
Chris Shorrock <cs@stripe.com>
2022-11-30 22:13:25 -0800

    day 1

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,5 +1,5 @@
 use std::fs;
-use itertools::Itertools;
+//use itertools::Itertools;
 
 fn main() {
     println!("Part 1 Result: {:?}", part1(&read_input_file()));
@@ -7,32 +7,46 @@ fn main() {
 }
 
 fn read_input_file() -> String {
-    fs::read_to_string("input.txt").expect("Oops - file could not be read")
+    fs::read_to_string("input.txt").expect("oops - file could not be read")
 }
 
-fn parse(input: &str) -> Vec<usize> {
-    input
-        .lines()
-        .map(|line| {line.parse::<usize>().unwrap()})
+fn parse(input: &str) -> Vec<Vec<usize>> {
+    let mut elfs: Vec<Vec<usize>> = vec![];
+    let mut current: Vec<usize> = vec![];
+    for line in input.lines() {
+        if line.trim().is_empty() {
+            elfs.push(current);
+            current = vec![];
+        } else {
+            current.push(line.trim().parse().expect(&format!("could not parse int from string: {}", line)));
+        }
+    }
+    elfs.push(current);
+    elfs
+}
+
+fn parse_calories(input: &str) -> Vec<usize> {
+    parse(input)
+        .iter()
+        .map(|elf| elf.iter().sum())
         .collect()
 }
 
 fn part1(input: &str) -> usize {
-    parse(input)
+    *parse_calories(input)
         .iter()
-        .tuple_windows()
-        .filter(|(first, second)| first < second)
-        .count()
+        .max()
+        .expect("could not determine max value, no elements?")
 }
 
 fn part2(input: &str) -> usize {
-    parse(input)
+    let mut calories = parse_calories(input);
+    calories.sort_by(|a, b| b.cmp(a));
+
+    calories
         .iter()
-        .tuple_windows()
-        .map(|(first, second, third)| first + second + third)
-        .tuple_windows()
-        .filter(|(first, second)| first < second)
-        .count()
+        .take(3)
+        .sum()
 }
 
 #[cfg(test)]
@@ -41,40 +55,44 @@ mod tests {
     use indoc::indoc;
 
     static EXAMPLE: &str = indoc! {"
-        199
-        200
-        208
-        210
-        200
-        207
-        240
-        269
-        260
-        263
+        1000
+        2000
+        3000
+
+        4000
+
+        5000
+        6000
+
+        7000
+        8000
+        9000
+
+        10000    
     "};
 
     #[test]
     fn test_part_1_example() {
         let result = part1(EXAMPLE);
-        assert_eq!(result, 7);
+        assert_eq!(result, 24000);
     }
 
     #[test]
     fn test_part_1() {
         let result = part1(&read_input_file());
-        assert_eq!(result, 1559);
+        assert_eq!(result, 71924);
     }
 
     #[test]
     fn test_part_2_example() {
         let result = part2(EXAMPLE);
-        assert_eq!(result, 5);
+        assert_eq!(result, 45000);
     } 
 
     #[test]
     fn test_part_2() {
         let result = part2(&read_input_file());
-        assert_eq!(result, 1600);
+        assert_eq!(result, 210406);
     }
 }
 
